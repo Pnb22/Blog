4afe73271b5bd32600e76a05861b592cc3cebf61
arthur fonseca <arthurfgp22@gmail.com>
2026-03-14 16:05:43 -0300

    3

--- blog/app/artigos/page.tsx
+++ blog/app/artigos/page.tsx
@@ -1,17 +1,17 @@
-import artigos from "../data/artigos.json"
 import Link from "next/link"
+import artigos from "@/data/artigos.json"
 
 export default function Home() {
   return (
     <main>
-      <h1>Blog de Artigos</h1>
+      <h1>Blog</h1>
 
       {artigos.map((artigo) => (
         <div key={artigo.slug}>
-          <h2>{artigo.titulo}</h2>
-          <p>{artigo.descricao}</p>
+          <h2>{artigo.title}</h2>
+          <p>{artigo.description}</p>
 
-          <Link href={`/artigos/${artigo.slug}`}>
+          <Link href={`/artigo/${artigo.slug}`}>
             Ler artigo
           </Link>
         </div>
